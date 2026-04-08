a66e27e61b511801442533ddf7af7fc4d14fbbc1
Neptune990 <460219@bsd48.org>
2026-04-08 09:07:30 -0700

    Initialize and basic steps

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main () {
+  return 0;
+}
+
+/*
+Steps to do:
+1. Understand how red-black trees work
+2. Create implementation of tree with parent pointer to the nodes
+3. Maintain tree so it hands numbers up to 3 digits, no 0 and tree updates no matter what you do to it
+4. Functions need to make are add(enter number to tree), read(read in a series of numbers from a file which should update tree properly after each one), print(print ree with number, red/black value, and parent)
+5. Debug and comments
+*/
